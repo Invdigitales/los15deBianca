acec63b04d3f3caad78cc9c36665c10167b6346c
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2024-06-12 19:18:30 -0300

    2

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3353.4023308052606!2d-61.581367900000004!3d-32.8081032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95c9e9bf85084001%3A0xe95051ce15c3f496!2sLa%20Fermina%20Club%20De%20Campo.!5e0!3m2!1ses-419!2sar!4v1715292113659!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3563.453102861918!2d-65.26280369999999!3d-26.7299133!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x942267c9ce34101b%3A0x7ead5549aa7700f2!2sParroquia%20Inmaculada%20Concepci%C3%B3n!5e0!3m2!1ses-419!2sar!4v1718228893124!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         
         
     })
